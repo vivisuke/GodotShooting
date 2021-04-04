0d3a3d89113fb739731cb7303783ba0bab4755c7
vivisuke <ntsuda@master.email.ne.jp>
2021-04-04 13:27:38 +0900

    0006 ◎ C 04/04 04/04 0.001│ToDo│自機：Space 押下時のみレイザー（Line2D）表示

--- GodotShooting/Main.gd
+++ GodotShooting/Main.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+const SCREEN_WIDTH = 500
+const SCREEN_HEIGHT = 800
+const LR_LIMIT = 32
+const FIGHTER_LEFT = LR_LIMIT
+const FIGHTER_RIGHT = SCREEN_WIDTH - LR_LIMIT
+
 func _ready():
 	pass # Replace with function body.
 
@@ -8,4 +14,10 @@ func _physics_process(delta):
 	if dx != 0:
 		#print(dx)
 		$Fighter.position.x += dx * 4
+		$Fighter.position.x = max(FIGHTER_LEFT, $Fighter.position.x)
+		$Fighter.position.x = min(FIGHTER_RIGHT, $Fighter.position.x)
+	if Input.is_action_pressed("ui_accept"):
+		$Fighter/Line2D.show()
+	else:
+		$Fighter/Line2D.hide()
 	pass
