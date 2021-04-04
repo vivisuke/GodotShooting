53249dd9736bbd521a46b7bb17bce8e7157a97ea
vivisuke <ntsuda@master.email.ne.jp>
2021-04-04 15:09:05 +0900

    0013 ◎ C 04/04 04/04 0.001│ToDo│敵機をスペースインベーダーのように並べてみる？

--- GodotShooting/Main.gd
+++ GodotShooting/Main.gd
@@ -5,8 +5,21 @@ const SCREEN_HEIGHT = 800
 const LR_LIMIT = 32
 const FIGHTER_LEFT = LR_LIMIT
 const FIGHTER_RIGHT = SCREEN_WIDTH - LR_LIMIT
+const ENEMY_Y0 = 100
+const ENEMY_DY = 64
+const ENEMY_X0 = 64
+const ENEMY_DX = 64
+
+var Enemy = load("res://Enemy.tscn")
 
 func _ready():
+	for i in range(5):
+		var py = ENEMY_Y0 + ENEMY_DY*i
+		for k in range(7):
+			var px = ENEMY_X0 + ENEMY_DX*k
+			var en = Enemy.instance()
+			add_child(en)
+			en.position = Vector2(px, py)
 	pass # Replace with function body.
 
 func _physics_process(delta):
